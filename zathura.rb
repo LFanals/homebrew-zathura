51fec32b4e998e88abf48162bb736146d912948f
LFanals <lfanalsb@gmail.com>
2022-09-01 07:49:37 +0200

    changes are commented

--- zathura.rb
+++ zathura.rb
@@ -37,17 +37,17 @@ class Zathura < Formula
     
     # Adding in the titlebar modifications
     
-    inreplace "zathura/zathura.c" do |s|
-      s.gsub! "GdkWindow* window = gtk_widget_get_window(zathura->ui.session->gtk.view);", "
-  GdkWindow* window = gtk_widget_get_window(zathura->ui.session->gtk.view);
-  GtkWidget* topLevelWidget = gtk_widget_get_toplevel(zathura->ui.session->gtk.view); // TopLevel is (in zathura) always a GtkWindow, so we just check to see if it is NULL to prevent crashing.
-  if (topLevelWidget == NULL) {
-    return;
-  }
-  if (GTK_IS_WINDOW(topLevelWidget)) {
-  	gtk_window_set_decorated(GTK_WINDOW(topLevelWidget), false);
-  }"
-    end
+#     inreplace "zathura/zathura.c" do |s|
+#       s.gsub! "GdkWindow* window = gtk_widget_get_window(zathura->ui.session->gtk.view);", "
+#   GdkWindow* window = gtk_widget_get_window(zathura->ui.session->gtk.view);
+#   GtkWidget* topLevelWidget = gtk_widget_get_toplevel(zathura->ui.session->gtk.view); // TopLevel is (in zathura) always a GtkWindow, so we just check to see if it is NULL to prevent crashing.
+#   if (topLevelWidget == NULL) {
+#     return;
+#   }
+#   if (GTK_IS_WINDOW(topLevelWidget)) {
+#   	gtk_window_set_decorated(GTK_WINDOW(topLevelWidget), false);
+#   }"
+#     end
 
 
     system 'mkdir build'
