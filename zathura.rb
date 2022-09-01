2a71fdda91b98621e428bdbe11fe1e7d3fdcefa3
LFanals <lfanalsb@gmail.com>
2022-09-01 20:49:18 +0200

    a:

--- zathura.rb
+++ zathura.rb
@@ -37,17 +37,16 @@ class Zathura < Formula
     
     # Adding in the titlebar modifications
     
-#     inreplace "zathura/zathura.c" do |s|
-#       s.gsub! "GdkWindow* window = gtk_widget_get_window(zathura->ui.session->gtk.view);", "
-#       GdkWindow* window = gtk_widget_get_window(zathura->ui.session->gtk.view);
-#       GtkWidget* topLevelWidget = gtk_widget_get_toplevel(zathura->ui.session->gtk.view); // TopLevel is (in zathura) always a GtkWindow, so we just check to see if it is NULL to prevent crashing.
-#       gtk_window_set_titlebar(GTK_WINDOW(topLevelWidget), gtk_header_bar_new()); // Casting GtkWindow to the GtkWidget to fit the function and creating a new (empty) titlebar.
-#         "
-#     end
+    inreplace "zathura/zathura.c" do |s|
+      s.gsub! "GdkWindow* window = gtk_widget_get_window(zathura->ui.session->gtk.view);", "
+      GdkWindow* window = gtk_widget_get_window(zathura->ui.session->gtk.view);
+      GtkWidget* topLevelWidget = gtk_widget_get_toplevel(zathura->ui.session->gtk.view); // TopLevel is (in zathura) always a GtkWindow, so we just check to see if it is NULL to prevent crashing.
+      gtk_window_set_titlebar(GTK_WINDOW(topLevelWidget), gtk_header_bar_new()); // Casting GtkWindow to the GtkWidget to fit the function and creating a new (empty) titlebar.
+        "
+    end
  
 
       # gtk_window_set_decorated(GTK_WINDOW(topLevelWidget), true);
-    #
 
 
     inreplace "data/zathura.css_t" do |s|
