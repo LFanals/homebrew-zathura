581ec9afa0e42388e4bef57df0c803b5040f56b5
LFanals <lfanalsb@gmail.com>
2022-09-01 20:23:09 +0200

    1123

--- zathura.rb
+++ zathura.rb
@@ -41,25 +41,22 @@ class Zathura < Formula
       s.gsub! "GdkWindow* window = gtk_widget_get_window(zathura->ui.session->gtk.view);", "
       GdkWindow* window = gtk_widget_get_window(zathura->ui.session->gtk.view);
       GtkWidget* topLevelWidget = gtk_widget_get_toplevel(zathura->ui.session->gtk.view); // TopLevel is (in zathura) always a GtkWindow, so we just check to see if it is NULL to prevent crashing.
-      gtk_window_set_decorated(GTK_WINDOW(topLevelWidget), true);"
+      gtk_window_set_titlebar(GTK_WINDOW(topLevelWidget), gtk_header_bar_new()); // Casting GtkWindow to the GtkWidget to fit the function and creating a new (empty) titlebar.
+        "
     end
 
+      # gtk_window_set_decorated(GTK_WINDOW(topLevelWidget), true);
 
-    
-#     inreplace "zathura/zathura.c" do |s|
-#       s.gsub! "GdkWindow* window = gtk_widget_get_window(zathura->ui.session->gtk.view);", "
-#   GdkWindow* window = gtk_widget_get_window(zathura->ui.session->gtk.view);
-#   GtkWidget* topLevelWidget = gtk_widget_get_toplevel(zathura->ui.session->gtk.view); // TopLevel is (in zathura) always a GtkWindow, so we just check to see if it is NULL to prevent crashing.
-#   gtk_window_set_decorated(GTK_WINDOW(topLevelWidget), false);
-#   "
-#     end
-#   if (topLevelWidget == NULL) {
-#     return;
-#   }
-#   if (GTK_IS_WINDOW(topLevelWidget)) {
-#   	gtk_window_set_decorated(GTK_WINDOW(topLevelWidget), false);
-#   }
-
+    inreplace "data/zathura.css_t" do |s|
+      s.gsub! "\#@session@ .indexmode:selected {", "
+        window {
+          border-radius: 10px;
+        }
+        \#@session@ .statusbar {
+          border-radius: 0px 0px 10px 10px; /* Rounding only the bottom corners to correlate with the window. */
+        }
+        \#@session@ .indexmode:selected {"
+    end
 
 
     system 'mkdir build'
