2dbef72beffb399eaf903d72c3655cebc1833665
LFanals <lfanalsb@gmail.com>
2022-09-04 06:36:52 +0200

    1000 by 1000 for widget, testing

--- zathura.rb
+++ zathura.rb
@@ -47,7 +47,8 @@ class Zathura < Formula
   gtk_window_set_decorated(GTK_WINDOW(topLevelWidget), true);
   // gtk_window_set_decorated(GTK_WINDOW(topLevelWidget), false);
   // gtk_window_set_titlebar(GTK_WINDOW(topLevelWidget), gtk_header_bar_new()); // Casting GtkWindow to the GtkWidget to fit the function and creating a new (empty) titlebar.
-  gtk_widget_set_size_request(topLevelWidget, 0, 0);
+  // gtk_widget_set_size_request(topLevelWidget, 0, 0);
+  gtk_widget_set_size_request(topLevelWidget, 1000, 1000);
   "
     end
 
