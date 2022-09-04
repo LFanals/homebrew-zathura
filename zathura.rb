dfae1376279df6c11af735ac283d49873b35bce8
LFanals <lfanalsb@gmail.com>
2022-09-04 06:25:08 +0200

    get widget, but try to change its size

--- zathura.rb
+++ zathura.rb
@@ -44,7 +44,7 @@ class Zathura < Formula
   if (topLevelWidget == NULL) {
     return;
   }
-  gtk_window_set_decorated(GTK_WINDOW(topLevelWidget), false);
+  // gtk_window_set_decorated(GTK_WINDOW(topLevelWidget), false);
   // gtk_window_set_titlebar(GTK_WINDOW(topLevelWidget), gtk_header_bar_new()); // Casting GtkWindow to the GtkWidget to fit the function and creating a new (empty) titlebar.
   gtk_widget_set_size_request(topLevelWidget, 0, 0);
   "
