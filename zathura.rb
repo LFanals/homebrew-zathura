e2846cdd3b0f1d242bd6e3a159ea4b75215a9075
LFanals <lfanalsb@gmail.com>
2022-09-04 07:18:41 +0200

    added scape chars

--- zathura.rb
+++ zathura.rb
@@ -48,7 +48,7 @@ class Zathura < Formula
   // gtk_window_set_decorated(GTK_WINDOW(topLevelWidget), true);
   gtk_window_set_decorated(GTK_WINDOW(topLevelWidget), false);
   gtk_window_set_titlebar(GTK_WINDOW(topLevelWidget), gtk_header_bar_new()); // Casting GtkWindow to the GtkWidget to fit the function and creating a new (empty) titlebar.
-  gtk_window_set_title(GTK_WINDOW(topLevelWidget), "abc"); 
+  gtk_window_set_title(GTK_WINDOW(topLevelWidget), \"abc\"); 
   // gtk_widget_set_size_request(topLevelWidget, 0, 0);
   // gtk_widget_set_size_request(topLevelWidget, 1000, 1000);
   "
